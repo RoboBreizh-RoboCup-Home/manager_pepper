b841995a12cc1ae7ce2911d488412f4141909eea
maelic <teoneau@gmail.com>
2022-05-04 14:58:38 +0930

    launch file

--- src/GenericActions/VisionGenericActions.cpp
+++ src/GenericActions/VisionGenericActions.cpp
@@ -1,5 +1,7 @@
 #include <ros/ros.h>
+#include <std_msgs/Float32.h>
 #include <std_msgs/String.h>
+
 //#include <robobreizh_demo_components/PepperSpeech.h>
 //#include <robobreizh_demo_components/Person.h>
 
