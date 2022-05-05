23dcf858f0d3e8cd6c387eafcaef4f122e56af98
maelic <teoneau@gmail.com>
2022-05-05 14:46:51 +0930

    some fixes

--- src/GenericActions/VisionGenericActions.cpp
+++ src/GenericActions/VisionGenericActions.cpp
@@ -61,7 +61,9 @@ namespace generic
         if (shared_msg != NULL)
         {
             msg = *shared_msg;
-            ROS_INFO("Door opened at distance  %f", float(2.1));
+            float test;
+            test = 2.1;
+            ROS_INFO("Door opened at distance ++ %f", test);
             ROS_INFO("Door opened at distance  %f", msg.data);
 
             return true;
