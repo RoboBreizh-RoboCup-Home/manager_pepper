93c68b90f625d3167e72845d0d461708c873a80b
tomkimsour <tomkimsour@hotmail.fr>
2022-06-15 12:10:49 +0930

    update sqlite db

--- manager_db/populate.sql
+++ manager_db/populate.sql
@@ -1,21 +1,21 @@
-INSERT INTO person (face_features)
-VALUES ("[2,2,3,3]" );
+-- Insert colors
+INSERT INTO color (label) 
+VALUES ("black"), ("white"), ("brown"), ("yellow"), ("blue"), ("green");
 
+-- Insert object type
+INSERT INTO object_type (label) 
+VALUES ("furniture"), ("kitcheware"), ("food"), ("drinks");
+
+-- Insert location
 INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz)
 VALUES ("instructionPoint","map", 2.092, -0.734, 0.000, 0.000, 0.000, 0.927, 0.376);
 INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz)
 VALUES ("arena","map",1.076, 0.770, 0.000,0.000, 0.000, 0.866, 0.499);
 
-
-INSERT INTO color (label) 
-VALUES ("black"), ("white"), ("brown"), ("yellow"), ("blue"), ("green");
-
-INSERT INTO object_type (label) 
-VALUES ("furniture"), ("kitcheware"), ("food"), ("drinks");
-
+-- Insert object
 INSERT INTO object (label, type, color) 
 VALUES ("can", 3, 1);
 
-
-INSERT INTO person (age, height, color, outfit)
+-- Insert person
+INSERT INTO person (age, height, color_id, outfit)
 VALUES (16,1.7,1,"sweater");
--- manager_db/roboBreizhDb.sql
+++ manager_db/roboBreizhDb.sql
@@ -1,30 +1,14 @@
-PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 COMMIT;
 
-CREATE TABLE IF NOT EXISTS person (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name TEXT,
-    favorite_drink TEXT,
-    age INTEGER,
-    height REAL,
-    color_id colors,
-    outfit TEXT,
-    face_features BLOB
-    FOREIGN KEY(color_id) REFERENCES colors(id),
-    FOREIGN KEY(position_name) REFERENCES location(name)
+CREATE TABLE color (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	label TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS object (
+CREATE TABLE object_type (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	label TEXT NOT NULL,
-	score REAL,
-	type object_type_id NOT NULL,
-	color_id colors,
-	position_name location,
-    FOREIGN KEY(object_type_id) REFERENCES colors(id),
-    FOREIGN KEY(color_id) REFERENCES colors(id),
-    FOREIGN KEY(position_name) REFERENCES location(name)
+	label TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS location (
@@ -40,12 +24,26 @@ CREATE TABLE IF NOT EXISTS location (
     angle REAL
 );
 
-CREATE TABLE color (
+CREATE TABLE IF NOT EXISTS object (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	label TEXT NOT NULL
+	label TEXT NOT NULL,
+	score REAL,
+	object_type_id INTEGER NOT NULL,
+	color_id INTEGER,
+	position_name INTEGER,
+    FOREIGN KEY(object_type_id) REFERENCES object_type(id),
+    FOREIGN KEY(color_id) REFERENCES colors(id),
+    FOREIGN KEY(position_name) REFERENCES location(name)
 );
 
-CREATE TABLE object_type (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	label TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS person (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT,
+    favorite_drink TEXT,
+    age INTEGER,
+    height REAL,
+    color_id INTEGER,
+    outfit TEXT,
+    face_features BLOB,
+    FOREIGN KEY(color_id) REFERENCES colors(id)
 );
