9f8c2dfe9adb78a786081fb6be38a4a8d2987dc2
tomkimsour <tomkimsour@hotmail.fr>
2022-06-28 11:25:23 +0930

    fix bad argument for vision plan

--- include/PlanHighLevelActions/VisionPlanActions.hpp
+++ include/PlanHighLevelActions/VisionPlanActions.hpp
@@ -15,7 +15,7 @@ namespace robobreizh
             void aWaitForDoorOpening(std::string params, bool* run);
             void aFindEmptySeat(std::string params, bool* run);
             void aFindHumanAndStoreFeatures(std::string params, bool* run);
-            void aFindHumanAndStoreFeaturesWithDistanceFilter(double param, bool* run);
+            void aFindHumanAndStoreFeaturesWithDistanceFilter(std::string params, bool* run);
         } // namespace plan
     } // namespace vision
 }// namespace robobreizh
