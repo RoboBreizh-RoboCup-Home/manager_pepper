3bcfbe79b3accea3e8f708bbe827ed37d315ec4a
SPQReL team <spqrel@googlegroups.com>
2022-06-30 05:33:12 +0000

    fix bug already fixed

--- src/DatabaseModel/VisionModel.cpp
+++ src/DatabaseModel/VisionModel.cpp
@@ -464,7 +464,7 @@ namespace robobreizh
             return objectList;
         }; 
 
-        std::vector<robobreizh::Person> VisionModel::getObjectsByLabel(){
+        std::vector<robobreizh::Person> VisionModel::getAllPerson(){
             query = "SELECT person.id, person.name, person.favorite_drink, person.gender, color_skin.label as skin_color_id, color_cloth.label as cloth_color_id, person.position_x, person.position_y, person.position_z, person.distance FROM person LEFT JOIN color color_cloth ON person.cloth_color_id = color_cloth.id LEFT JOIN color color_skin ON person.skin_color_id = color_skin.id";
             pStmt = nullptr;
             int rc;
