b52459a9d68bfec2174ddc4d667a88b341569991
maelic <teoneau@gmail.com>
2022-07-10 20:15:32 +0930

    debug

--- include/GenericActions/VisionGenericActions.hpp
+++ include/GenericActions/VisionGenericActions.hpp
@@ -23,10 +23,10 @@ namespace robobreizh
 			bool addObjectToDatabase(robobreizh::Object obj);
 			bool addPersonToDatabase(robobreizh::Person person);
 			bool isInRadius(float x1,float y1,float z1,float x2,float y2,float z2,float epsilon);
-            bool findObjectRange(std::string object, geometry_msgs::Point coord);
+            std::string findObjectRange(std::string object, geometry_msgs::Point coord);
             std::string findAndLocateLastObjectPose();
             std::string findObjectCategory();
-            std::string findObjectRange();
+            bool WaitForHumanWaivingHand()
 			geometry_msgs::Point convertOdomToMap(float x,float y, float z);
         } // namespace generic
     } // namespace vision
