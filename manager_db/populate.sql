1ba194ac17999eb3503f4f5032afcf241633f39f
teoneau <teoneau@gmail.com>
2022-07-12 10:57:29 +0000

    m

---
+++ manager_db/populate.sql
@@ -0,0 +1,311 @@
+-- Insert colors
+-- Thanks Vim macros :D
+INSERT INTO color(id,label) VALUES (1,'Black');
+INSERT INTO color(id,label) VALUES (2,'White');
+INSERT INTO color(id,label) VALUES (3,'Red');
+INSERT INTO color(id,label) VALUES (4,'Lime');
+INSERT INTO color(id,label) VALUES (5,'Blue');
+INSERT INTO color(id,label) VALUES (6,'Yellow');
+INSERT INTO color(id,label) VALUES (7,'Cyan / Aqua');
+INSERT INTO color(id,label) VALUES (8,'Magenta / Fuchsia');
+INSERT INTO color(id,label) VALUES (9,'Silver');
+INSERT INTO color(id,label) VALUES (10,'Gray');
+INSERT INTO color(id,label) VALUES (11,'Maroon');
+INSERT INTO color(id,label) VALUES (12,'Olive');
+INSERT INTO color(id,label) VALUES (13,'Green');
+INSERT INTO color(id,label) VALUES (14,'Purple');
+INSERT INTO color(id,label) VALUES (15,'Teal');
+INSERT INTO color(id,label) VALUES (16,'Navy');
+INSERT INTO color(id,label) VALUES (17,'maroon');
+INSERT INTO color(id,label) VALUES (18,'dark red');
+INSERT INTO color(id,label) VALUES (19,'brown');
+INSERT INTO color(id,label) VALUES (20,'firebrick');
+INSERT INTO color(id,label) VALUES (21,'crimson');
+INSERT INTO color(id,label) VALUES (22,'red');
+INSERT INTO color(id,label) VALUES (23,'tomato');
+INSERT INTO color(id,label) VALUES (24,'coral');
+INSERT INTO color(id,label) VALUES (25,'indian red');
+INSERT INTO color(id,label) VALUES (26,'light coral');
+INSERT INTO color(id,label) VALUES (27,'dark salmon');
+INSERT INTO color(id,label) VALUES (28,'salmon');
+INSERT INTO color(id,label) VALUES (29,'light salmon');
+INSERT INTO color(id,label) VALUES (30,'orange red');
+INSERT INTO color(id,label) VALUES (31,'dark orange');
+INSERT INTO color(id,label) VALUES (32,'orange');
+INSERT INTO color(id,label) VALUES (33,'gold');
+INSERT INTO color(id,label) VALUES (34,'dark golden rod');
+INSERT INTO color(id,label) VALUES (35,'golden rod');
+INSERT INTO color(id,label) VALUES (36,'pale golden rod');
+INSERT INTO color(id,label) VALUES (37,'dark khaki');
+INSERT INTO color(id,label) VALUES (38,'khaki');
+INSERT INTO color(id,label) VALUES (39,'olive');
+INSERT INTO color(id,label) VALUES (40,'yellow');
+INSERT INTO color(id,label) VALUES (41,'yellow green');
+INSERT INTO color(id,label) VALUES (42,'dark olive green');
+INSERT INTO color(id,label) VALUES (43,'olive drab');
+INSERT INTO color(id,label) VALUES (44,'lawn green');
+INSERT INTO color(id,label) VALUES (45,'chartreuse');
+INSERT INTO color(id,label) VALUES (46,'green yellow');
+INSERT INTO color(id,label) VALUES (47,'dark green');
+INSERT INTO color(id,label) VALUES (48,'green');
+INSERT INTO color(id,label) VALUES (49,'forest green');
+INSERT INTO color(id,label) VALUES (50,'lime');
+INSERT INTO color(id,label) VALUES (51,'lime green');
+INSERT INTO color(id,label) VALUES (52,'light green');
+INSERT INTO color(id,label) VALUES (53,'pale green');
+INSERT INTO color(id,label) VALUES (54,'dark sea green');
+INSERT INTO color(id,label) VALUES (55,'medium spring green');
+INSERT INTO color(id,label) VALUES (56,'spring green');
+INSERT INTO color(id,label) VALUES (57,'sea green');
+INSERT INTO color(id,label) VALUES (58,'medium aqua marine');
+INSERT INTO color(id,label) VALUES (59,'medium sea green');
+INSERT INTO color(id,label) VALUES (60,'light sea green');
+INSERT INTO color(id,label) VALUES (61,'dark slate gray');
+INSERT INTO color(id,label) VALUES (62,'teal');
+INSERT INTO color(id,label) VALUES (63,'dark cyan');
+INSERT INTO color(id,label) VALUES (64,'aqua');
+INSERT INTO color(id,label) VALUES (65,'cyan');
+INSERT INTO color(id,label) VALUES (66,'light cyan');
+INSERT INTO color(id,label) VALUES (67,'dark turquoise');
+INSERT INTO color(id,label) VALUES (68,'turquoise');
+INSERT INTO color(id,label) VALUES (69,'medium turquoise');
+INSERT INTO color(id,label) VALUES (70,'pale turquoise');
+INSERT INTO color(id,label) VALUES (71,'aqua marine');
+INSERT INTO color(id,label) VALUES (72,'powder blue');
+INSERT INTO color(id,label) VALUES (73,'cadet blue');
+INSERT INTO color(id,label) VALUES (74,'steel blue');
+INSERT INTO color(id,label) VALUES (75,'corn flower blue');
+INSERT INTO color(id,label) VALUES (76,'deep sky blue');
+INSERT INTO color(id,label) VALUES (77,'dodger blue');
+INSERT INTO color(id,label) VALUES (78,'light blue');
+INSERT INTO color(id,label) VALUES (79,'sky blue');
+INSERT INTO color(id,label) VALUES (80,'light sky blue');
+INSERT INTO color(id,label) VALUES (81,'midnight blue');
+INSERT INTO color(id,label) VALUES (82,'navy');
+INSERT INTO color(id,label) VALUES (83,'dark blue');
+INSERT INTO color(id,label) VALUES (84,'medium blue');
+INSERT INTO color(id,label) VALUES (85,'blue');
+INSERT INTO color(id,label) VALUES (86,'royal blue');
+INSERT INTO color(id,label) VALUES (87,'blue violet');
+INSERT INTO color(id,label) VALUES (88,'indigo');
+INSERT INTO color(id,label) VALUES (89,'dark slate blue');
+INSERT INTO color(id,label) VALUES (90,'slate blue');
+INSERT INTO color(id,label) VALUES (91,'medium slate blue');
+INSERT INTO color(id,label) VALUES (92,'medium purple');
+INSERT INTO color(id,label) VALUES (93,'dark magenta');
+INSERT INTO color(id,label) VALUES (94,'dark violet');
+INSERT INTO color(id,label) VALUES (95,'dark orchid');
+INSERT INTO color(id,label) VALUES (96,'medium orchid');
+INSERT INTO color(id,label) VALUES (97,'purple');
+INSERT INTO color(id,label) VALUES (98,'thistle');
+INSERT INTO color(id,label) VALUES (99,'plum');
+INSERT INTO color(id,label) VALUES (100,'violet');
+INSERT INTO color(id,label) VALUES (101,'magenta / fuchsia');
+INSERT INTO color(id,label) VALUES (102,'orchid');
+INSERT INTO color(id,label) VALUES (103,'medium violet red');
+INSERT INTO color(id,label) VALUES (104,'pale violet red');
+INSERT INTO color(id,label) VALUES (105,'deep pink');
+INSERT INTO color(id,label) VALUES (106,'hot pink');
+INSERT INTO color(id,label) VALUES (107,'light pink');
+INSERT INTO color(id,label) VALUES (108,'pink');
+INSERT INTO color(id,label) VALUES (109,'antique white');
+INSERT INTO color(id,label) VALUES (110,'beige');
+INSERT INTO color(id,label) VALUES (111,'bisque');
+INSERT INTO color(id,label) VALUES (112,'blanched almond');
+INSERT INTO color(id,label) VALUES (113,'wheat');
+INSERT INTO color(id,label) VALUES (114,'corn silk');
+INSERT INTO color(id,label) VALUES (115,'lemon chiffon');
+INSERT INTO color(id,label) VALUES (116,'light golden rod yellow');
+INSERT INTO color(id,label) VALUES (117,'light yellow');
+INSERT INTO color(id,label) VALUES (118,'saddle brown');
+INSERT INTO color(id,label) VALUES (119,'sienna');
+INSERT INTO color(id,label) VALUES (120,'chocolate');
+INSERT INTO color(id,label) VALUES (121,'peru');
+INSERT INTO color(id,label) VALUES (122,'sandy brown');
+INSERT INTO color(id,label) VALUES (123,'burly wood');
+INSERT INTO color(id,label) VALUES (124,'tan');
+INSERT INTO color(id,label) VALUES (125,'rosy brown');
+INSERT INTO color(id,label) VALUES (126,'moccasin');
+INSERT INTO color(id,label) VALUES (127,'navajo white');
+INSERT INTO color(id,label) VALUES (128,'peach puff');
+INSERT INTO color(id,label) VALUES (129,'misty rose');
+INSERT INTO color(id,label) VALUES (130,'lavender blush');
+INSERT INTO color(id,label) VALUES (131,'linen');
+INSERT INTO color(id,label) VALUES (132,'old lace');
+INSERT INTO color(id,label) VALUES (133,'papaya whip');
+INSERT INTO color(id,label) VALUES (134,'sea shell');
+INSERT INTO color(id,label) VALUES (135,'mint cream');
+INSERT INTO color(id,label) VALUES (136,'slate gray');
+INSERT INTO color(id,label) VALUES (137,'light slate gray');
+INSERT INTO color(id,label) VALUES (138,'light steel blue');
+INSERT INTO color(id,label) VALUES (139,'lavender');
+INSERT INTO color(id,label) VALUES (140,'floral white');
+INSERT INTO color(id,label) VALUES (141,'alice blue');
+INSERT INTO color(id,label) VALUES (142,'ghost white');
+INSERT INTO color(id,label) VALUES (143,'honeydew');
+INSERT INTO color(id,label) VALUES (144,'ivory');
+INSERT INTO color(id,label) VALUES (145,'azure');
+INSERT INTO color(id,label) VALUES (146,'snow');
+INSERT INTO color(id,label) VALUES (147,'black');
+INSERT INTO color(id,label) VALUES (148,'dim gray / dim grey');
+INSERT INTO color(id,label) VALUES (149,'gray / grey');
+INSERT INTO color(id,label) VALUES (150,'dark gray / dark grey');
+INSERT INTO color(id,label) VALUES (151,'silver');
+INSERT INTO color(id,label) VALUES (152,'light gray / light grey');
+INSERT INTO color(id,label) VALUES (153,'gainsboro');
+INSERT INTO color(id,label) VALUES (154,'white smoke');
+INSERT INTO color(id,label) VALUES (155,'white');
+
+
+-- Insert object type
+INSERT INTO object_type (label) 
+VALUES ("furniture"), ("kitcheware"), ("food"), ("drinks");
+
+-- Insert location
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("instruction point","map",  3.013, 6.985, 0.000,0.000, 0.000, 0.412, 0.911, 0.849);
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("arena","map",1.822, 6.775, 0.000, 0.000, 0.000, -0.574, 0.819,-1.223);
+--Setting goal: Frame:map, Position(1.822, 6.775, 0.000), Orientation(0.000, 0.000, -0.574, 0.819) = Angle: -1.223
+-- INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+-- VALUES ("living room","map",1.440, 0.254, 0.000,0.000, 0.000, 0.749, 0.662,1.694);
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("pos a","map", 3.013, 6.985, 0.000,0.000, 0.000, 0.412, 0.911, 0.849);
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("pos b","map", -0.795, 6.707, 0.000, 0.000, 0.000, -0.857, 0.515,-2.059);
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("pos c","map", -1.452, 3.834, 0.000, 0.000, 0.000, -0.830, 0.558,-1.957);
+
+--"init pose" Position(2.798, 1.652, 0.000), Orientation(0.000, 0.000, 0.719, 0.696) = Angle: 1.603
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("entry1","map", 3.050, 1.645, 0.000,0.000, 0.000, 0.647, 0.762, 1.408);
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("living room","map", 3.354,6.078, 0.000, 0.000,  0.000, -0.023, 1.000,  -0.047); 
+-- living room Setting goal: Frame:map, Position(1.611, 4.414, 0.000), Orientation(0.000, 0.000, -0.014, 1.000) = Angle: -0.028
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("kitchen","map", 4.561,10.950, 0.000, 0.000,  0.000, 0.998, 0.060,  3.022); 
+-- kitchen Setting goal: Frame:map, Position(3.525, 7.394, 0.000), Orientation(0.000, 0.000, 0.794, 0.608) = Angle: 1.834
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("bedroom","map", -0.969, 9.893, 0.000, 0.000,  0.000, -0.007, 1.000, -0.014); 
+--bedroom Setting goal: Frame:map, Position(-2.137, 7.223, 0.000), Orientation(0.000, 0.000, 0.331, 0.944) = Angle: 0.674
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("office","map", -0.494,7.073, 0.000, 0.000,  0.000, 0.470, 0.883,  0.978); 
+--office Setting goal: Frame:map, Position(0.183, 4.538, 0.000), Orientation(0.000, 0.000, -0.976, 0.219) = Angle: -2.701
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("exit","map",-1.687, 3.418, 0.000, 0.000,  0.000, -0.894, 0.448,  -2.213); 
+-- Setting goal: Frame:map, Position(-1.687, 3.418, 0.000), Orientation(0.000, 0.000, -0.894, 0.448) = Angle: -2.213
+
+-- FURNITURES LOCATION
+
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("sofa","map",3.354,6.078, 0.000, 0.000,  0.000, -0.023, 1.000,  -0.047); 
+-- Setting goal: Frame:map, Position(3.354, 6.078, 0.000), Orientation(0.000, 0.000, -0.023, 1.000) = Angle: -0.047
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("couch table","map",3.354,6.078, 0.000, 0.000,  0.000, -0.023, 1.000,  -0.047); 
+-- Setting goal: Frame:map, Position(3.354, 6.078, 0.000), Orientation(0.000, 0.000, -0.023, 1.000) = Angle: -0.047
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("side table","map",4.561,7.732, 0.000, 0.000,  0.000, -0.015, 1.000,  -0.029); 
+-- Setting goal: Frame:map, Position(4.561, 7.732, 0.000), Orientation(0.000, 0.000, -0.015, 1.000) = Angle: -0.029
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("dinner table","map",4.561,10.950, 0.000, 0.000,  0.000, 0.998, 0.060,  3.022); 
+-- Setting goal: Frame:map, Position(4.561, 10.950, 0.000), Orientation(0.000, 0.000, 0.998, 0.060) = Angle: 3.022
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("fridge","map",4.887,11.547, 0.000, 0.000,  0.000, 0.814, 0.580,  1.903); 
+-- Setting goal: Frame:map, Position(4.887, 11.547, 0.000), Orientation(0.000, 0.000, 0.814, 0.580) = Angle: 1.903
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("desk","map",-0.494,7.073, 0.000, 0.000,  0.000, 0.470, 0.883,  0.978); 
+--Setting goal: Frame:map, Position(-0.494, 7.073, 0.000), Orientation(0.000, 0.000, 0.470, 0.883) = Angle: 0.978
+
+
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("small shelf","map",1.535,11.984, 0.000, 0.000,  0.000, 0.710, 0.704,  1.579); 
+--Setting goal: Frame:map, Position(1.535, 11.984, 0.000), Orientation(0.000, 0.000, 0.710, 0.704) = Angle: 1.579
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("cupboard","map",-1.447,12.314, 0.000, 0.000,  0.000, 1.000, 0.009,  -3.123); 
+--Setting goal: Frame:map, Position(-1.447, 12.314, 0.000), Orientation(0.000, 0.000, -1.000, 0.009) = Angle: -3.123
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("big shelf","map",-1.345,10.857, 0.000, 0.000,  0.000,  1.000, 0.002,  3.138); 
+--Setting goal: Frame:map, Position(-1.345, 10.857, 0.000), Orientation(0.000, 0.000, 1.000, 0.002) = Angle: 3.138
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("kitchen bin","map",5.223,12.189, 0.000, 0.000,  0.000,  0.438,  0.899,  0.906); 
+--Setting goal: Frame:map, Position(5.223, 12.189, 0.000), Orientation(0.000, 0.000, 0.438, 0.899) = Angle: 0.906
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("bin","map",-0.476,6.054, 0.000, 0.000,  0.000, -0.006,  1.000,  -0.011); 
+--Setting goal: Frame:map, Position(-0.476, 6.054, 0.000), Orientation(0.000, 0.000, -0.006, 1.000) = Angle: -0.011
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("book shelf","map",2.159,7.412, 0.000, 0.000,  0.000, 0.703, 0.712,  1.558); 
+--Setting goal: Frame:map, Position(2.159, 7.412, 0.000), Orientation(0.000, 0.000, 0.703, 0.712) = Angle: 1.558
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("coat rack","map", 2.246, 5.369, 0.000, 0.000,  0.000, -0.951,  0.308, -2.515); 
+--Setting goal: Frame:map, Position(2.246, 5.369, 0.000), Orientation(0.000, 0.000, -0.951, 0.308) = Angle: -2.515
+
+
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("kitchen shelf","map",5.098,10.396, 0.000, 0.000,  0.000, -0.629, 0.777,  -1.361); 
+--Setting goal: Frame:map, Position(5.098, 10.396, 0.000), Orientation(0.000, 0.000, -0.629, 0.777) = Angle: -1.361
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("pantry","map",1.889,9.483, 0.000, 0.000,  0.000, 1.000, 0.020,  3.101); 
+-- Setting goal: Frame:map, Position(1.889, 9.483, 0.000), Orientation(0.000, 0.000, 1.000, 0.020) = Angle: 3.101
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("washing machine","map",1.982,11.896, 0.000, 0.000,  0.000,0.412, 0.911,  0.850); 
+--Setting goal: Frame:map, Position(1.982, 11.896, 0.000), Orientation(0.000, 0.000, 0.412, 0.911) = Angle: 0.850
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("sink","map", 1.503, 11.698, 0.000, 0.000,  0.000, 0.719, 0.695,  1.604); 
+--Setting goal: Frame:map, Position(1.503, 11.698, 0.000), Orientation(0.000, 0.000, 0.719, 0.695) = Angle: 1.604
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("bed","map", -0.969, 9.893, 0.000, 0.000,  0.000, -0.007, 1.000, -0.014); 
+--Setting goal: Frame:map, Position(-0.969, 9.893, 0.000), Orientation(0.000, 0.000, -0.007, 1.000) = Angle: -0.014
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("show rack","map", -1.619, 5.651, 0.000, 0.000,  0.000, -0.999, 0.038,  -3.065); 
+--Setting goal: Frame:map, Position(-1.619, 5.651, 0.000), Orientation(0.000, 0.000, -0.999, 0.038) = Angle: -3.065
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("starting pose1","map", -1.619, 5.651, 0.000, 0.000,  0.000, -0.999, 0.038,  -3.065); 
+--Setting goal: Frame:map, Position(1.653, 7.597, 0.000), Orientation(0.000, 0.000, -0.381, 0.925) = Angle: -0.782
+ -- Receptionist, GPSR: starting pose1
+
+INSERT INTO location (name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("pose findMyMates","map", 2.164, 5.212, 0.000, 0.000,  0.000, 0.540, 0.842,  1.141); 
+--Setting goal: Frame:map, Position(2.164, 5.212, 0.000), Orientation(0.000, 0.000, 0.540, 0.842) = Angle: 1.141
+
+
+-- Insert sub location
+INSERT INTO location (sub_location_name,location_name,frame,x,y,z,qw,qx,qy,qz,angle)
+VALUES ("table1","arena","map",-0.740, -1.721, 0.000, 0.000, 0.000, 0.987, 0.159, 2.823),
+("table2","arena","map",-0.784, -1.372, 0.000, 0.000, 0.000, 0.908, 0.419, 2.276),
+("table computers","arena","map",-0.784, -1.372, 0.000, 0.000, 0.000, 0.908, 0.419, 2.276),
+("blackboard","living room","map",1.147, 0.099, 0.000, 0.000, 0.000, -0.281, 0.960, -0.570);
+-- Insert object
+insert into object (label,color_id,position_x,position_y,position_z,distance) values ("can",2,0.2,0.2,0.2,1.2);
+
+-- Insert person
+INSERT INTO person (gender, age,cloth_color_id, skin_color_id)
+VALUES ("male","25-30",1,1);
+INSERT INTO person (name,favorite_drink,gender, age,cloth_color_id, skin_color_id)
+VALUES ("Carl","wine","male","25-30",1,1);
+
+INSERT INTO seated_person (person_id)
+VALUES (8),(5);
