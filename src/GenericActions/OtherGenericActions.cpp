7d46866fdb46dd3bc5a5ed1d618fb262c805a390
mbbarange <mbbarange@gmail.com>
2022-07-12 18:09:39 +0200

    [add] String Utility Functions

--- src/GenericActions/OtherGenericActions.cpp
+++ src/GenericActions/OtherGenericActions.cpp
@@ -2,9 +2,10 @@
 #include <std_msgs/String.h>
 //#include <robobreizh_demo_components/PepperSpeech.h>
 //#include <robobreizh_demo_components/Person.h>
-
+#include <vector>
+#include <string>
 #include <boost/thread/thread.hpp>
-
+#include <boost/algorithm/string.hpp>
 #include "GenericActions/OtherGenericActions.hpp"
 
 using namespace std;
@@ -36,6 +37,88 @@ namespace generic
             return false;
         }
     }
+    
+
+     bool isValidObject(string objName){
+
+        std::vector<string> objects;
+        objects.push_back("Water");
+        objects.push_back("Milk");
+        objects.push_back("Coke");
+        objects.push_back("Tonic");
+        objects.push_back("Bubble Tea");
+        objects.push_back("Ice tea");
+        objects.push_back("Cloth");
+        objects.push_back("Sponge");
+        objects.push_back("Cleaner");
+        objects.push_back("Corn Flakes");
+        objects.push_back("Tuna Can");
+        objects.push_back("Sugger");
+        objects.push_back("Mustard");
+        objects.push_back("Apple");
+        objects.push_back("Peach");
+        objects.push_back("Orange");
+        objects.push_back("Banana");
+        objects.push_back("Strawberry");
+        objects.push_back("Pockys");
+        objects.push_back("Pringles");
+        objects.push_back("Spoon");
+        objects.push_back("Fork");
+        objects.push_back("Plate");
+        objects.push_back("Bowl");
+        objects.push_back("Mug");
+        objects.push_back("Knife");
+
+         for (auto obj: objects) {
+             std::string lowerObj = boost::to_upper_copy(obj);
+             std::string lowerobjName = boost::to_upper_copy(objName);
+             bool found = boost::algorithm::contains(lowerObj, lowerobjName);
+            if(found ){
+                return true;
+            }
+        }
+     }
+
+
+    bool isValidPlaces(string placeName)
+    {
+
+        std::vector<string> Places;
+        Places.push_back("House Plant");        
+        Places.push_back("Coat Rack");
+        Places.push_back("Sofa");
+        Places.push_back("Couch Table");
+        Places.push_back("TV");
+        Places.push_back("Side Table");
+        Places.push_back("Book Shelf");
+        Places.push_back("Pantry");
+        Places.push_back("Dinner Table");
+        Places.push_back("Kitchen Bin");
+        Places.push_back("Fridge");
+        Places.push_back("Washing Machine");
+        Places.push_back("Sink");
+        Places.push_back("Small Shelf");
+        Places.push_back("Cupboard");
+        Places.push_back("Big Shelf");
+        Places.push_back("Bed");
+        Places.push_back("Desk");
+        Places.push_back("Show Rack");
+        Places.push_back("Bin");
+        Places.push_back("Office Shelf");
+
+         for (auto place: Places) {
+             std::string lowerPlace = boost::to_upper_copy(place);
+             std::string lowerPlaceName = boost::to_upper_copy(placeName);
+             bool found = boost::algorithm::contains(lowerPlace, lowerPlaceName);
+            if(found ){
+                return true;
+            }
+        }
+     }
+
+
+
+
 } // namespace generic
 } // namespace other
 } // namespace robobreizh
