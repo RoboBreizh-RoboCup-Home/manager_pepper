9cd084eaff31a45d89943b8d02ceac1938e792c4
Mael Bouabdelli <mael.bouabdelli@gmail.com>
2022-07-15 10:44:24 +0700

    [FIX] Compilation Error

--- src/PlanHighLevelActions/InitiailisationPlanActions.cpp
+++ src/PlanHighLevelActions/InitiailisationPlanActions.cpp
@@ -348,7 +348,7 @@ void aInitWhereIsThis(string params, bool* run)
     ROS_INFO("2.9 Where Is This? - initialisation");
 
     bool ret;
-
+    
     const string name_whereisthis_furniture = "param_whereisthis_furniture";
     std_msgs::String param_whereisthis_furniture;
     param_whereisthis_furniture.data = "None";
