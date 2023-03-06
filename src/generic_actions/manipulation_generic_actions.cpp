71a1a0d594a0f035a5016af928006088cda19952
SPQReL team <spqrel@googlegroups.com>
2023-03-06 23:16:04 +0000

    rise right arm

--- src/generic_actions/manipulation_generic_actions.cpp
+++ src/generic_actions/manipulation_generic_actions.cpp
@@ -24,26 +24,33 @@ bool dropObject(string hand) {
   return true;
 }
 bool bendArm(string arm) {
-  int argc = 0;
+  int argc = 2;
   char** argv = nullptr;
+  argv = new char*[argc];
+  argv[0]="--qi-url";
+  argv[1]="tcp://localhost:9559";
+
 
   if (arm == "Right") {
-    ROS_LOG("Bending right arm");
     qi::ApplicationSession app(argc, argv);
+    app.start();
     qi::SessionPtr session = app.session();
+    ROS_INFO("Bending right arm session created");
     // move right arm using ALMotion service
     qi::AnyObject al_motion = session->service("ALMotion");
-    std::vector<std::string> names = { "RShoulderRoll", "RShoulderPitch", " RElbowYaw", "RElbowRoll", "RWristYaw" };
+    ROS_INFO("Bending right arm session connected to almotion");
+    std::vector<std::string> names = { "RShoulderRoll", "RShoulderPitch", "RElbowYaw", "RElbowRoll", "RWristYaw" };
     std::vector<float> angles = { 0.0f, 0.8f, 1.55f, 1.1f, 1.5f };
     float fractionMaxSpeed = 0.1f;
 
-    al_motion.call<void>("setStiffnesses", 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
+    std::vector<float> stiffness= { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+    al_motion.call<void>("setStiffnesses", names ,stiffness);
+    ROS_INFO("Bending right arm session set stiffness");
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     al_motion.call<void>("setAngles", names, angles, fractionMaxSpeed);
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    al_motion.call<void>("setStiffnesses", 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
-
-    app.run();
+    stiffness = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f}; 
+    al_motion.call<void>("setStiffnesses", names,stiffness);
   }
 
   return true;
