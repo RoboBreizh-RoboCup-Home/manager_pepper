6e0e4c08462785aaa723fd6ed526833058591c29
khksamuel <83699297+khksamuel@users.noreply.github.com>
2023-05-18 15:25:25 +0930

    Revert "Vision generic action" (#62)

--- include/vision_utils.hpp
+++ include/vision_utils.hpp
@@ -10,9 +10,7 @@ namespace robobreizh {
 void objectMsgToObjectStruct(robobreizh::database::Object* object, robobreizh_msgs::Object objectMsg,
                              geometry_msgs::Point coord);
 void personMsgToPersonStruct(robobreizh::database::Person* person, robobreizh_msgs::Person pers,
-                             geometry_msgs::Point coord);
-void personMsgToPersonPoseStruct(robobreizh::database::Person* person, robobreizh_msgs::Person pers,
-                                 robobreizh_msgs::PersonPose persPose, geometry_msgs::Point coord);
+                             robobreizh_msgs::PersonPose persPose, geometry_msgs::Point coord);
 std::vector<std_msgs::String> fillTabMsg(std::vector<std::string> detections);
 int isInForbiddenRoom(float x, float y);
 bool addPersonToDatabase(robobreizh::database::Person person);
