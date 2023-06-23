b16778b9fc274b97f4bdbcb0f5511d87bcd7c5d6
Louis <manhim1218@gmail.com>
2023-06-23 04:57:54 +0000

    add take obj to person function in dialog

--- src/robobreizh_manager.cpp
+++ src/robobreizh_manager.cpp
@@ -82,6 +82,8 @@ public:
     register_action("DialogAskOperatorHelp", &dialog::aAskOperatorHelpOrder);
     register_action("DialogIntroduceAtoB", &dialog::aIntroduceAtoB);
     register_action("DialogOfferSeatToHuman", &dialog::aOfferSeatToHuman);
+    register_action("DialogTakeObjectToPerson", &dialog::aTakeObjectToPerson);
+
 #ifdef LEGACY
     register_action("DialogChitChat", &dialog::aDialogChitChat);
 #endif
